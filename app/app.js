970f9f634e7196a2c503c43e49cf48a4bdc1f92e
Peter Vilja <peter.vilja@gmail.com>
2015-02-18 10:36:46 +0200

    add keywords stream

--- app/app.js
+++ app/app.js
@@ -4,16 +4,14 @@ var Tweet = require('./tweet');
 
 var tweet = Tweet();
 var stream = tweet.stream();
+var keywords = tweet.keywords();
 
 http.createServer((req, res) => {
-  if (req.url == '/tweets') {
-    res.writeHead(200, {
-      'Content-Type' : 'text/event-stream',
-      'Cache-Control' : 'no-cache',
-      'Connection' : 'keep-alive'
-    });
-
-    stream.on('data', (data) => res.write(`data: ${data}\n\n`));
-    // setInterval(() => res.write(`data: test\n\n`), 1000);
-  }
+  res.writeHead(200, {
+    'Content-Type' : 'text/event-stream',
+    'Cache-Control' : 'no-cache',
+    'Connection' : 'keep-alive'
+  });
+  if (req.url == '/tweets') stream.on('data', (data) => res.write(`data: ${data}\n\n`));
+  if (req.url == '/keywords') keywords.on('data', (data) => res.write(`data: ${data}\n\n`));
 }).listen(4000);
--- app/tweet.js
+++ app/tweet.js
@@ -8,6 +8,7 @@ var consumerSecret = process.env.FRPSECRET;
 var accessTokenKey = process.env.FRPACCESSKEY;
 var accessTokenSecret = process.env.FRPACCESSSECRET;
 var buffer = '';
+var buf = '';
 
 function Tweet() {
   if (!(this instanceof Tweet)) return new Tweet();
@@ -37,6 +38,29 @@ Tweet.prototype.stream = function () {
     while ((index = buffer.indexOf('\r\n')) > -1) {
       var obj = buffer.slice(0, index);
       buffer = buffer.slice(index + 2);
+      if (obj.length > 0) readable.push(obj);
+    }
+  }));
+  request.end();
+  return readable;
+};
+
+Tweet.prototype.keywords = function () {
+  var keywords = [
+    'happy', 'joyful', 'excited', 'relaxed', 'awesome',
+    'sad', 'depressed', 'bored', 'angry', 'hurt'
+  ];
+  var readable = new stream.Readable({objectMode: true});
+  readable._read = () => {};
+  var url = 'https://stream.twitter.com/1.1/statuses/filter.json?track=' + keywords.join(',');
+  var request = this.oa.post(url, accessTokenKey, accessTokenSecret, {});
+
+  request.on('response', response => response.on('data', chunk => {
+    var index;
+    buf += chunk.toString();
+    while ((index = buf.indexOf('\r\n')) > -1) {
+      var obj = buf.slice(0, index);
+      buf = buf.slice(index + 2);
       if (obj.length > 0) readable.push(obj);
     }
   }));
