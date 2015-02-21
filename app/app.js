ca8b709439db57e8ebe852aeae740ad6ddfdc748
Peter Vilja <peter.vilja@gmail.com>
2015-02-21 21:19:27 +0200

    keep open connections in a set and write data to each connection when it arrives

--- app/app.js
+++ app/app.js
@@ -5,6 +5,11 @@ var Tweet = require('./tweet');
 var tweet = Tweet();
 var stream = tweet.stream();
 var keywords = tweet.keywords();
+var subscribers = new Set();
+var keywordSubscribers = new Set();
+
+stream.on('data', data => subscribers.forEach(res => res.write(`data: ${data}\n\n`)));
+keywords.on('data', data => keywordSubscribers.forEach(res => res.write(`data: ${data}\n\n`)));
 
 http.createServer((req, res) => {
   res.writeHead(200, {
@@ -12,6 +17,12 @@ http.createServer((req, res) => {
     'Cache-Control' : 'no-cache',
     'Connection' : 'keep-alive'
   });
-  if (req.url == '/tweets') stream.on('data', (data) => res.write(`data: ${data}\n\n`));
-  if (req.url == '/keywords') keywords.on('data', (data) => res.write(`data: ${data}\n\n`));
+  if (req.url == '/tweets') {
+    subscribers.add(res);
+    res.on('close', () => subscribers.delete(res));
+  }
+  if (req.url == '/keywords') {
+    keywordSubscribers.add(res);
+    res.on('close', () => keywordSubscribers.delete(res));
+  }
 }).listen(4000);
